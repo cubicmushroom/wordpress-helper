db706851334baaaf8f0796c418a22cb948180203
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-09-04 17:52:39 +0100

    Update to store post_type arguments in object properties , & set labels in constructor.

--- src/CM/WP/Element/PostType.php
+++ src/CM/WP/Element/PostType.php
@@ -28,6 +28,7 @@ if (!class_exists('CM_WP_Element_PostType')) {
          * @param CM_WP_Base $owner The the plugin/theme object that this belongs to
          * @param string     $name  Slug for the post being registered (will be
          *                          pre-fixed by the plugin prefix)
+         *                          Name should be the plural term
          * @param array      $slug  (optional) The slug for the post.  Will be
          *                          prefixed  by the plugin/theme prefix.
          *                          If not provided, will be created from the $name
@@ -95,10 +96,28 @@ if (!class_exists('CM_WP_Element_PostType')) {
         protected $slug;
 
         /**
-         * Name of the post type (label)
-         * @var string
+         * Labels for the post type
+         * @var array
          */
-        protected $name;
+        protected $labels = array();
+
+        /**
+         * Default register_post_type arguments
+         * @var array
+         */
+        protected $post_args = array(
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true, 
+            'show_in_menu' => true, 
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'book' ),
+            'capability_type' => 'post',
+            'has_archive' => true, 
+            'hierarchical' => false,
+            'menu_position' => null,
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+        );
 
         /**
          * Sets the object slug & name (label)
@@ -109,9 +128,54 @@ if (!class_exists('CM_WP_Element_PostType')) {
          */
         protected function __construct( CM_WP_Base $owner, $slug, $name )
         {
-            $this->owner = $owner;
-            $this->slug = $slug;
-            $this->name  = $name;
+            $this->owner  = $owner;
+            $this->slug   = $slug;
+
+            // Build the labels array from the $name
+            $this->set_labels( $name );
+            
+        }
+
+
+        protected function set_labels( $plural_label, $single_label = null ) {
+
+            if ( is_null( $single_label ) ) {
+                $single_label = preg_replace('/s$/', '', $plural_label);
+            }
+
+            $this->labels = array(
+                'name'               => $plural_label,
+                'singular_name'      => $single_label,
+                'add_new'            => "Add New {$single_label}",
+                'add_new_item'       => "Add New {$single_label}",
+                'edit_item'          => "Edit {$single_label}",
+                'new_item'           => "New {$single_label}",
+                'all_items'          => "All {$plural_label}",
+                'view_item'          => "View {$single_label}",
+                'search_items'       => "Search {$plural_label}",
+                'not_found'          => sprintf(
+                    "No %s found",
+                    strtolower($plural_label)
+                ),
+                'not_found_in_trash' => sprintf(
+                    "No %s found in Trash",
+                    strtolower($plural_label)
+                ),
+                'parent_item_colon'  => "",
+                'menu_name'          => $plural_label,
+            );
+        }
+
+        /**
+         * Builds the register_post_type arguments form the object properties
+         * 
+         * @return array
+         */
+        protected function prepare_post_args() {
+            $args = $this->post_args;
+            $args['labels'] = $this->labels;
+
+            return $args;
         }
 
         /**
