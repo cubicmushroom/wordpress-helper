7dc7620ef37a7c5ab32ebe2f16692fec59473b95
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-09-04 19:11:49 +0100

    Added code to add rewrite to affect permalinks.

--- src/CM/WP/Element/Rewrite.php
+++ src/CM/WP/Element/Rewrite.php
@@ -4,6 +4,13 @@ if (!class_exists('CM_WP_Element_Rewrite')) {
 
     class CM_WP_Element_Rewrite {
 
+
+        /*************
+         * Constants *
+         *************/
+
+        const REDIRECT_ID_PARAM = 'rewrite_id';
+
         /*************************
          * Static factory method *
          *************************/
@@ -41,7 +48,12 @@ if (!class_exists('CM_WP_Element_Rewrite')) {
             $position = 'top'
         ) {
 
-            $rewrite = new 
+            $rewrite = new CM_WP_Element_Rewrite(
+                $owner,
+                $regex,
+                $redirect,
+                $position
+            );
 
             self::$rewrites[$rewrite->get_id()] = $rewrite;
 
@@ -64,6 +76,24 @@ if (!class_exists('CM_WP_Element_Rewrite')) {
          * @var [type]
          */
         protected $id;
+
+        /**
+         * Regex for the rewrite rule
+         * @var string
+         */
+        protected $regex;
+
+        /**
+         * Redirect string for the rewrite rule
+         * @var string
+         */
+        protected $redirect;
+
+        /**
+         * The position for registering the rewrite rule
+         * @var string
+         */
+        protected $position;
 
         /**
          * Generates a unique ID for the rewrite & adds the rewrite rule
@@ -94,13 +124,61 @@ if (!class_exists('CM_WP_Element_Rewrite')) {
             // Generate a unique ID for the rewrite
             $this->id = md5( $regex );
 
+            $this->regex = $regex;
+
             // Add the redirect's ID as a parameter to the redirect.  This allows us
             // to hook into request when it's received & handle the request using a
             // callback set using the is_handles_by() method
-            $redirect = add_query_arg( array( self::, $id), $redirect );
+            $redirect_id_param = "{$this->owner->get_prefix()}_" . 
+                self::REDIRECT_ID_PARAM;
+            $this->redirect = add_query_arg(
+                array( $redirect_id_param => $this->id),
+                $redirect
+            );
 
-            // Register the rewrite rule
-            add_rewrite_rule( $regex, $redirect, $position );
+            $this->position = $position;
+
+            add_action( 'init', array( $this, 'add_rewrite_rule_to_wp' ) );
+        }
+
+        /**
+         * Called during the init action hook to register rewrite rules
+         *
+         * Uses an additional function call, so that this can be called during plugin
+         * activation before flushing permalinks
+         *
+         * @return void
+         */
+        public function hook_add_rewrite_rule_to_wp() {
+            $this->add_rewrite_rule_to_wp();
+        }
+
+        /**
+         * Adds the rewrite rule to wordpress
+         *
+         * Called during plugin activation or init hook
+         *
+         * @return void
+         */
+        protected function add_rewrite_rule_to_wp() {
+            add_rewrite_rule( $this->regex, $this->redirect, $this->position );
+        }
+
+
+
+
+
+        /**************************
+         * Getters, setters, etc. *
+         **************************/
+
+        /**
+         * Returns the ID of the redirect
+         * 
+         * @return string
+         */
+        public function get_id() {
+            return $this->id;
         }
     }
 }
